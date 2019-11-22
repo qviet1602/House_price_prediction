94f5f43f65445a6a98f2a847f815cce94ba44c29
saiguttikonda <saiguttikonda@level11.com>
2019-11-21 19:58:28 -0800

    Deleting model and serializer. Not using it

--- domicilian/median_prices/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class MedianPrice(models.Model):
-    name = models.CharField(max_length=100, null=False)
-    state_code = models.CharField(max_length=2, null=False)
-    list_price = models.IntegerField()
--- domicilian/median_prices/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import MedianPrice
-
-class MedianPriceSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MedianPrice
-        fields = ("name", "state_code", "list_price")
